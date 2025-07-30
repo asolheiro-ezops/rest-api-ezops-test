8a69f3c3d1c4a8e4de7f43ed41d8d991f425b54f
Armando Solheiro <avgsolheiro@gmail.com>
2025-07-30 17:41:09 -0300

    build: all interfaces

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.get('/health', (req, res) => {
 	res.status(200).send('OK');
 });
 
-app.listen(3000, () => {
+app.listen(3000, '0.0.0.0', () => {
 	console.log('Servidor iniciado na porta 3000');
 });
 
